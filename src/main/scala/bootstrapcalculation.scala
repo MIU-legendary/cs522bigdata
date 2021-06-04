4961fa5f6af18e8e39f006e991327949c51506cb
Thành Lê <thanhle01101995@gmail.com>
2021-06-04 13:32:07 -0500

    Update cái nhẹ

--- src/main/scala/bootstrapcalculation.scala
+++ src/main/scala/bootstrapcalculation.scala
@@ -1,47 +1,87 @@
 
-import java.io._
 import org.apache.spark._
 
+import java.io._
 
-object testingsort extends App {
+
+case class Person(name: String, age: Int)
+
+case class VocabularyPerPerson(num: String, year: String, sex: String, edu: String, vocab: String)
+
+case class result(cato: String, mean: Double, variance: Double)
+
+object bootstrapcalculation extends App {
   val conf = new SparkConf().setAppName("Spark Sort").setMaster("local")
   val sc = new SparkContext(conf)
 
-  sc.setLogLevel("WARN")
+  // Exploring SparkSQL
+  // Initialize an SQLContext
+  val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+  import sqlContext._
+  import sqlContext.implicits._
+  import org.apache.spark.sql.functions._
 
-  var myOne = List(List("cat", "mat", "bat"), List("hat", "mat", "rat"), List("cat", "mat", "sat"), List("cat", "fat", "bat"))
-  myOne.foreach(println)
-  var myTwo = sc.parallelize(myOne)
-  myTwo.foreach(println)
-  var myThree = myTwo.map(x => (x(1), x(2)))
-  myThree.foreach(println)
-  var myFour = myThree.sortBy(_._1, false).sortBy(_._2)
-  myFour.foreach(println)
+
+  sc.setLogLevel("WARN")
 
   val writer = new PrintWriter(new File("result.txt"))
 
-
   writer.write("Hello World")
 
-  var tf = sc.textFile("data/dataFile.txt")
-  var counts = tf.flatMap(line => line.split("\\s+")).map(x => (x.toLowerCase(), 1)).reduceByKey(_ + _).sortByKey(true)
-  counts.foreach(println)
+  var csv = sc.textFile("DATA/scala_input/Vocab.csv")
 
-  val res = counts.collect()
-  for (n <- res) writer.println(n.toString())
+  var headerAndRows = csv.map(line => line.split(",").map(_.trim))
+  val header = headerAndRows.first
 
-  writer.close()
+  val vocalData = headerAndRows.filter(_(0) != header(0))
+  val vocabulary = vocalData
+    .map(c => VocabularyPerPerson(c(0), c(1), c(2), c(3), c(4)))
+    .toDF
 
-  val sqlcontext = new org.apache.spark.sql.SQLContext(sc)
+  vocabulary.printSchema()
 
-  val dfs = sqlcontext.read.json("data/Emp.json")
+  vocabulary.select("edu").show(5)
 
-  dfs.show()
-  dfs.printSchema()
-  val dfs1 = dfs.filter(!dfs("name").isNull)
-  dfs1.show()
-  dfs1.select("name").show()
-  dfs1.filter(dfs("age") > 23).show()
-  dfs1.groupBy("age").count().show()
+  val sample = vocabulary.sample(withReplacement = true,0.25)
+
+  sample.show()
+
+  sample.groupBy("sex").agg(avg("vocab")).show()
+
+  val time = 0
+  val pairVocab = new Pair(0,0)
+  val pairEdu = new Pair(0,0)
+  for (time <- 1 to 1000){
+    val resample = sample.sample(withReplacement = false, 1)
+    resample.
+      foreach(x => {
+        val todouble = x.getAs("edu").toString.toDouble
+        println(todouble)
+      })
+  }
+
+
+
+//
+//  header.foreach(println)
+//  creditCardData.first().foreach(println)
+
+
+//  val res = counts.collect()
+//  for (n <- res) writer.println(n.toString())
+//
+//  writer.close()
+//
+//  val sqlcontext = new org.apache.spark.sql.SQLContext(sc)
+//
+//  val dfs = sqlcontext.read.json("data/Emp.json")
+//
+//  dfs.show()
+//  dfs.printSchema()
+//  val dfs1 = dfs.filter(!dfs("name").isNull)
+//  dfs1.show()
+//  dfs1.select("name").show()
+//  dfs1.filter(dfs("age") > 23).show()
+//  dfs1.groupBy("age").count().show()
 
 }
